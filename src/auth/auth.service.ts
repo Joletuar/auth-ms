2d81366578a7653f48ef272787d042270f764c22
joletuar <joletuar.dev@gmail.com>
2024-07-31 22:31:30 -0500

    feat: add prisma cliento to perform querys

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+
+import { PrismaClient } from '@prisma/client';
+
+import { LogingUserDto } from './dtos/login.user.dto';
+import { RegisterUserDto } from './dtos/register.user.dto';
 
 @Injectable()
-export class AuthService {
-  registerUser() {
-    return 'registerUser';
+export class AuthService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger('AuthService');
+
+  async onModuleInit() {
+    await this.$connect();
+
+    this.logger.log('Connected to MongoDB');
   }
 
-  loginUser() {
-    return 'loginUser';
+  loginUser(loginUserDto: LogingUserDto) {
+    return loginUserDto;
+  }
+
+  registerUser(registerUserDto: RegisterUserDto) {
+    return registerUserDto;
   }
 
   verifyToken() {

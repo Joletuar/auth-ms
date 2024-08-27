49dc0c3a2dcdc61fdd6601317a22dc1245de6a6a
joletuar <joletuar.dev@gmail.com>
2024-08-26 22:28:15 -0500

    dep: add pino and set as a default logger for application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,28 @@
 import { Module } from '@nestjs/common';
+import { LoggerModule } from 'nestjs-pino';
+
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
+        transport:
+          process.env.NODE_ENV !== 'production'
+            ? {
+                target: 'pino-pretty',
+                options: {
+                  messageKey: 'message',
+                  colorize: true,
+                },
+              }
+            : undefined,
+        messageKey: 'message',
+      },
+    }),
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Logger as PinoLogger } from 'nestjs-pino';
 
 import { AppModule } from './app.module';
 import { envs } from './config/envs';
@@ -9,6 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
+      bufferLogs: true,
       transport: Transport.RMQ,
       options: {
         queue: 'auth',
@@ -21,6 +23,8 @@ async function bootstrap() {
       },
     },
   );
+
+  app.useLogger(app.get(PinoLogger));
 
   app.useGlobalPipes(
     new ValidationPipe({
